dfb6a4f354429192767fa21c900eed09ceac60b8
mewtek <april.p@mistwx.com>
2025-06-17 01:15:26 -0700

    Remove unused using declarations

--- i2Receiver/Program.cs
+++ i2Receiver/Program.cs
@@ -1,14 +1,13 @@
-﻿using System.Runtime.CompilerServices;
-using i2Receiver.Communication;
+﻿using i2Receiver.Communication;
 
 namespace i2Receiver
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+        static async Task Main()
         {
             Console.Title = "wxstar.dev | IntelliStar 2 Data Receiver - Init..";
-            Config config = Config.Load();
+            Config.Load();
             Log.SetLogLevel(Config.config.LogLevel);
 
             // Create the temp directory if it does not exist 
